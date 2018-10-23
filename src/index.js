9db61453336d848ae7964123e45d74b354c66a3b
ri7nz <ri7nz.labs@gmail.com>
2018-10-23 20:38:55 +0700

    Base Code

---
+++ src/index.js
@@ -0,0 +1,88 @@
+/**
+ * Rel List;
+ *   'dns-prefetch',
+ *   'prefetch',
+ *   'preconnect',
+ *   'preload', require as attributes
+ *
+ * @url https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types
+ *
+ */
+
+const REL = ['dns-prefetch', 'prefetch', 'preconnect', 'preload']
+
+/**
+ * Simple Injection HTML Script resource | Edotensei
+ * Example use:
+ *     const scriptList = [
+ *      {
+ *          src: 'url',
+ *          async: boolean,
+ *          defer: boolean,
+ *          rel: 'preload|prefetch'
+ *      }
+ *     ];
+ *
+ *     Edotensei.addScript(scriptList)
+ *     Edotensei.removeScript(scriptList)
+ */
+export default class Edotensei {
+  /**
+   * @param {array} scriptList
+   */
+  static add(scriptList) {
+    scriptList.forEach((script, key) => {
+      script.id = `script:${key}`
+      Edotensei.append({...script})
+    })
+  }
+
+  /**
+   * @param {array} scriptList
+   */
+  static remove(scriptList) {
+    scriptList.forEach((script, key) => {
+      script = document.getElementById(`script:${key}`)
+      script.parentNode.removeChild(script)
+    })
+  }
+
+  /**
+   * @param {object} attributes
+   * {
+   *    id  : string,
+   *    src : string <url>,
+   *    async : boolean,
+   *    defer : boolean,
+   *    rel   : string <HTML:Link_types>
+   *    reference for rel https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types
+   * }
+   */
+  static append({id, src, async, defer, rel}) {
+    const element = document.createElement('script')
+
+    Object.assign(element, {id, src, async, defer})
+
+    if (async && defer) {
+      element.async = undefined
+      element.defer = undefined
+    }
+
+    if (rel && REL.includes(rel)) {
+      const link = document.createElement('link')
+      const attributes = {
+        href: src,
+        rel,
+      }
+
+      if (rel.toLowerCase() === 'preload') {
+        Object.assign(attributes, {as: 'script'})
+      }
+
+      Object.assign(link, attributes)
+      document.head.appendChild(link)
+    }
+
+    document.body.appendChild(element)
+  }
+}
