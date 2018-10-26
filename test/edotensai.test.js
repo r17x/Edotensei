7e9e2ee2a8ee5c3249c933c5a52c1e34acb0ae5b
ri7nz <ri7nz.labs@gmail.com>
2018-10-27 00:05:17 +0700

    Add Test with Jest, Update Babelrc, Update package.json

---
+++ test/edotensai.test.js
@@ -0,0 +1,11 @@
+import Edotensei from '../src'
+
+describe('check add & remove static method in edotensei', () => {
+  test('add in edotensei', () => {
+    expect(Edotensei.add).toBeTruthy()
+  })
+
+  test('remove in edotensei', () => {
+    expect(Edotensei.add).toBeTruthy()
+  })
+})
