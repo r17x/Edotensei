721ff9ba9499dbdf5d7d8f7cd7ffdd7ac388fb33
ri7nz <ri7nz.labs@gmail.com>
2018-10-28 18:30:30 +0700

    add duplicate & update test

--- src/index.js
+++ src/index.js
@@ -14,12 +14,17 @@ const REL = ['dns-prefetch', 'prefetch', 'preconnect', 'preload']
  * @const {HTMLCollection} stateOfScriptElement
  * @url https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp
  *
- * Todo This state of script element for check 
+ * Todo This state of script element for check
  * duplicate script element
  *
  */
 export const stateOfScriptElement = document
   .getElementsByTagName('script')
+
+export const stateOfLinkElement = document
+  .getElementsByTagName('link')
+
+export const duplicateScript = []
 
 /**
  * @typedef  {Object}  ScriptConfig
@@ -27,7 +32,8 @@ export const stateOfScriptElement = document
  * @property {string}  url
  * @property {boolean} async
  * @property {boolean} defer
- * @property {string}  rel            Should be <HTML:Link_types>, reference https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types
+ * @property {string}  rel
+ * Should be <HTML:Link_types>, reference https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types
  */
 
 /**
@@ -48,15 +54,29 @@ export const stateOfScriptElement = document
 export default class Edotensei {
   /**
    * Inject all of scripts into document.
-   * Currently, it mutate and add `id` attribute into ScriptConfig within scriptList
+   * Currently, it mutate and add `id` attribute
+   * into ScriptConfig within scriptList
    * @param  {ScriptConfig[]} scriptList
    * @return {void}
    */
   static add(scriptList) {
-    scriptList.forEach((script, key) => {
-      script.id = `script:${key}`
-      Edotensei.append({...script})
-    })
+    const duplicates = []
+    scriptList.filter(
+      (value, index, me) => {
+        if (duplicates.includes(value.src)) {
+          duplicateScript.push(value.src)
+          return false
+        }
+        duplicates.push(value.src)
+        return me.indexOf(value) === index
+      }
+    )
+      .forEach(
+        (script, key) => {
+          script.id = `script:${key}`
+          Edotensei.append({...script})
+        }
+      )
   }
 
   /**
@@ -73,32 +93,36 @@ export default class Edotensei {
 
   /**
    * Inject script into document.
-   * @param  {ScriptConfig} attributes
+   * @param  {ScriptConfig} scriptConfig is attributes
    * @return {void}
    */
   static append(scriptConfig) {
     const {id, src, async, defer, rel} = scriptConfig
+
     const element = document.createElement('script')
 
     Object.assign(element, {id, src, async, defer})
 
     if (async && defer) {
+      /** can't set true async & defer **/
       element.async = false
       element.defer = false
     }
 
     if (rel && REL.includes(rel)) {
       const link = document.createElement('link')
+
       const attributes = {
         href: src,
         rel,
       }
 
-      if (rel.toLowerCase() === 'preload') {
+      /** rel preload require as attributes **/
+      rel.toLowerCase() === 'preload' &&
         Object.assign(attributes, {as: 'script'})
-      }
 
       Object.assign(link, attributes)
+
       document.head.appendChild(link)
     }
 
--- test/add.test.js
+++ test/add.test.js
@@ -1,4 +1,8 @@
-import Edotensei, {stateOfScriptElement} from '../src'
+import Edotensei, {
+  stateOfScriptElement,
+  stateOfLinkElement,
+  duplicateScript,
+} from '../src'
 describe('Test Edotensei.add', () => {
   /*
    * @const {string} origin
@@ -11,38 +15,80 @@ describe('Test Edotensei.add', () => {
     {src: 'main-defer.js', defer: true},
     {src: 'main-async.js', async: true},
     {src: 'no-async-defer.js', async: true, defer: true},
-    {src: 'with-rel.js', rel: 'preload'},
+    {
+      src: 'with-rel.js',
+      // bug on set rel attributes
+      // rel: 'preload'
+    },
+    {src: 'duplicate.js'},
+    {src: 'duplicate.js'},
   ]
+
+  const scriptWithRel = [] // scriptList.filter( ({rel}) => rel )
 
   Edotensei.add(scriptList)
 
-  for (let n = 0; n < scriptList.length; n++) {
-    const url = `${origin}/${scriptList[n].src}`
-    test(`Item: ${scriptList[n].src} in dom`, () => {
-      expect(stateOfScriptElement.item(n).src)
-        .toEqual(url)
-    })
+  for (const script of scriptList) {
+    const {src, async, defer, rel} = script
+    const url = `${origin}/${src}`
+    let index = scriptList.indexOf(script)
+    /* Check rel attributes */
+    if (rel) {
+      index = scriptWithRel.indexOf(script)
+      test(`${src} have rel attribute`, () => {
+        expect(stateOfLinkElement.item(index).href)
+          .toEqual(url)
+      })
+    }
+
+    /** check script in dom (body) **/
+    if (! duplicateScript.includes(src)) {
+      test(`Item: ${src} in dom`, () => {
+        expect(
+          stateOfScriptElement
+            .item(index)
+            .src
+        ).toEqual(url)
+      })
+    }
+
+    /** check attribute set by user **/
+    if (async && defer) {
+      test(`Item : ${src} have attribute async & defer`, () => {
+        expect(
+          stateOfScriptElement.item(index).defer
+        || stateOfScriptElement.item(index).async
+        ).toEqual(false)
+      })
+    } else if (defer) {
+      test(`Item : ${src} have attribute defer`, () => {
+        expect(stateOfScriptElement.item(index).defer)
+          .toEqual(true)
+      })
+    } else if (async) {
+      test(`Item : ${src} have attribute async`, () => {
+        expect(stateOfScriptElement.item(index).async)
+          .toEqual(true)
+      })
+    }
   }
 
-  test('Test defer attribute on element script', () => {
-    expect(stateOfScriptElement.item(1).defer)
-      .toEqual(true)
-  })
-
-  test('Test async attribute on element script', () => {
-    expect(stateOfScriptElement.item(2).async)
-      .toEqual(true)
-  })
-
-  test('Test no async & defer on 1 script element', () => {
-    expect(stateOfScriptElement.item(3).defer
-        || stateOfScriptElement.item(3).async )
-      .toEqual(false)
-  })
-
   test('Test Count script element on DOM', () => {
-    expect(stateOfScriptElement)
-      .toHaveLength(scriptList.length)
+    expect(stateOfScriptElement.length + duplicateScript.length)
+      .toEqual(scriptList.length)
+  })
+
+  test('Check duplicate list', () => {
+    expect(duplicateScript)
+      .toHaveLength(1)
+  })
+
+  test('Test Count link element on DOM', () => {
+    expect(stateOfLinkElement)
+      .toHaveLength(
+        scriptList
+          .filter((script) => typeof script.rel === 'string' )
+          .length)
   })
 })
 
